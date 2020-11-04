2989b746ed85f5bccd1f7f78090aa1f2eeaab889
zh <zhzhzh@zhs-MacBook-Pro.local>
2020-11-05 00:23:29 +0800

    Try 01 knapsack problem

---
+++ algorithm/knapsack_test.go
@@ -0,0 +1,41 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func maxInt(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func knapsack01(W []int, V []int, cap int) int {
+	n := len(W) - 1
+	m := make([][]int, n+1)
+	for i := range m {
+		m[i] = make([]int, cap+1)
+	}
+	for i := range m[0] {
+		m[0][i] = 0
+	}
+	for i := 1; i <= n; i++ {
+		m[i][0] = 0
+		for j := 1; j <= cap; j++ {
+			if j >= W[i] {
+				m[i][j] = maxInt(m[i-1][j], m[i-1][j-W[i]]+V[i])
+			} else {
+				m[i][j] = m[i-1][j]
+			}
+		}
+	}
+	return m[n][cap]
+}
+
+func TestKnapsack01(t *testing.T) {
+	W := []int{-1, 1, 2, 3, 4}
+	V := []int{-1, 2, 3, 6, 8}
+	fmt.Println(knapsack01(W, V, 5))
+}
