8d25eb550c0329df825e2f8731c3b122132c6b94
bluecover <gurity@163.com>
2020-11-18 00:57:33 +0800

    Iterative array: factorial

--- csp/factorial_test.go
+++ csp/factorial_test.go
@@ -10,7 +10,7 @@ import (
 // > "Problem: Compute a factorial by the recursive method, to a given
 // limit."
 
-func fac(p chan int, q chan int, r chan int, o chan int) {
+func factorial(p chan int, q chan int, r chan int, o chan int) {
 	for n := range p {
 		if n == 1 {
 			o <- 1
@@ -23,33 +23,32 @@ func fac(p chan int, q chan int, r chan int, o chan int) {
 	}
 }
 
-func fac0(n int) int {
+func factorial0(n int) int {
 	p1 := make(chan int)
 	q1 := make(chan int)
 	r1 := make(chan int)
 	o1 := make(chan int)
-	go fac(p1, q1, r1, o1)
+	go factorial(p1, q1, r1, o1)
 
 	p2 := q1
 	q2 := make(chan int)
 	r2 := make(chan int)
 	o2 := r1
-	go fac(p2, q2, r2, o2)
+	go factorial(p2, q2, r2, o2)
 
 	p3 := q2
 	q3 := make(chan int)
 	r3 := make(chan int)
 	o3 := r2
-	go fac(p3, q3, r3, o3)
+	go factorial(p3, q3, r3, o3)
 
 	p4 := q3
 	o4 := r3
-	go fac(p4, nil, nil, o4)
+	go factorial(p4, nil, nil, o4)
 
 	p1 <- n
 	return <-o1
 }
-
 func Test_fac0(t *testing.T) {
-	fmt.Println(fac0(4))
+	fmt.Println(factorial0(4))
 }
--- csp/iterative_array_test.go
+++ csp/iterative_array_test.go
@@ -27,3 +27,32 @@ func addBy1(n int) int {
 func Test_addBy1(t *testing.T) {
 	fmt.Println(addBy1(100))
 }
+
+func fac(n int) int {
+	chs := make([]chan int, n+1)
+	for i := 0; i < n+1; i++ {
+		chs[i] = make(chan int)
+	}
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			for n := range chs[i] {
+				fmt.Printf("channel[%d] read %d\n", i, n)
+				if n == 1 {
+					chs[i] <- 1
+					break
+				}
+				if n > 1 {
+					chs[i+1] <- n - 1
+					r := <-chs[i+1]
+					chs[i] <- n * r
+				}
+			}
+		}(i)
+	}
+	chs[0] <- n
+	return <-chs[0]
+}
+
+func Test_fac(t *testing.T) {
+	fmt.Println(fac(5))
+}
