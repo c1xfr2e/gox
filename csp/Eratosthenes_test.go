244bb3529129c6f30d5554d46dcfa85712f7e1d0
bluecover <gurity@163.com>
2020-11-17 16:23:27 +0800

    Eratosthenes concurrent

--- csp/Eratosthenes_test.go
+++ csp/Eratosthenes_test.go
@@ -6,9 +6,9 @@ import (
 	"testing"
 )
 
-// Eratosthenes returns prime numbers <= n in a slice.
+// EratosthenesPlain returns prime numbers <= n in a slice.
 // phi is the count of prime numbers approximate to x/ln(x).
-func Eratosthenes(n int) []int {
+func EratosthenesPlain(n int) []int {
 	phi := int(math.Ceil((float64(n) / math.Log(float64(n)) * 1.2)))
 	p := make([]int, phi)
 	p[0], p[1] = 2, 3
@@ -33,7 +33,45 @@ func Eratosthenes(n int) []int {
 	return p[:k]
 }
 
-func TestEratosthenes(t *testing.T) {
-	e := Eratosthenes(100000)
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func generate(ch chan<- int) {
+	ch <- 2
+	for i := 3; ; i += 2 {
+		ch <- i // Send 'i' to channel 'ch'.
+	}
+}
+
+// Copy the values from channel 'in' to channel 'out',
+// removing those divisible by 'prime'.
+func filter(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in // Receive value from 'in'.
+		if i%prime != 0 {
+			out <- i // Send 'i' to 'out'.
+		}
+	}
+}
+
+// The prime sieve: Daisy-chain filter processes.
+func EratosthenesConcurrent() {
+	ch := make(chan int) // Create a new channel.
+	go generate(ch)      // Launch Generate goroutine.
+	for i := 0; i < 10; i++ {
+		prime := <-ch
+		print(prime, "\n")
+		ch1 := make(chan int)
+		go filter(ch, ch1, prime)
+		ch = ch1
+	}
+	print(<-ch, "\n")
+	print(<-ch, "\n")
+}
+
+func TestEratosthenesPlain(t *testing.T) {
+	e := EratosthenesPlain(100000)
 	fmt.Println(len(e))
+}
+
+func TestEratosthenesConcurrent(t *testing.T) {
+	EratosthenesConcurrent()
 }
