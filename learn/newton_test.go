0d75f7a70a168e795df1661bd4b6bd72c4592bac
bluecover <gurity@163.com>
2021-04-16 13:06:33 +0800

    Reanming variable in newton sqrt

--- learn/newton_test.go
+++ learn/newton_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestNewtonSqrt(t *testing.T) {
-	x := 256.0
-	p := 1.0
-	q := 0.0
+	c := 256.0
+	x := 1.0
+	p := 0.0
 	PREC := 0.000000000000001
-	for !(p-q > -PREC && p-q < PREC) {
-		q, p = p, p-(p*p-x)/(p*2)
+	for !(x-p > -PREC && x-p < PREC) {
+		p, x = x, x-(x*x-c)/(x*2)
 	}
-	fmt.Println(p)
+	fmt.Println(x)
 }
